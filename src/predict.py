d35fc792335c85e4b3ef75e66a84356150c54fb0
Garvit Gandhi <garvitgandhi0313@gmail.com>
2026-03-30 23:39:03 +0530

    Implemented prediction feature in predict.py

--- src/predict.py
+++ src/predict.py
@@ -7,3 +7,7 @@ print("Starting Prediction...")
 def load_model():
     "Load the trained model from the specified file.""
     model_path = "model.pkl"
+    print(f"Load models from '{model_path}'...")
+    model = joblib.load(model_path)
+    print("Model loaded successfully.")
+    return model
