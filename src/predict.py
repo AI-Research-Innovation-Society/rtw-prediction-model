5d6f5c8a138624a99474e6009bfc96ffc776fb00
Garvit Gandhi <garvitgandhi0313@gmail.com>
2026-03-30 23:32:53 +0530

    Started working on load_model

--- src/predict.py
+++ src/predict.py
@@ -1,1 +1,9 @@
+import pandas as pd 
+import joblib
+import argparse
 
+print("Starting Prediction...")
+
+def load_model():
+    "Load the trained model from the specified file.""
+    model_path = "model.pkl"
