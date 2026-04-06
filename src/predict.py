e6f057f5c41a97184802f91a7177a48fdf5a041d
Garvit Gandhi <garvitgandhi0313@gmail.com>
2026-04-06 09:27:41 +0530

    Added claims and predictions

--- src/predict.py
+++ src/predict.py
@@ -1,12 +1,31 @@
+import pandas as pd
 import joblib
+import argparse
+
+print("🚀 Starting the prediction robot...")
 
 def load_model():
-    """Load the trained model from the default 'model.pkl' file."""
+    """Load the trained model from the specified file."""
     model_path = "model.pkl"
-    print(f"Load models from '{model_path}'...")
+    print(f"Loading model from '{model_path}'...")
     model = joblib.load(model_path)
-    print("Model loaded successfully.")
+    print("Model successfully loaded.")
     return model
+
+def read_claims(file_path):
+    print(f"Reading the claim papers from: {file_path}")
+    df = pd.read_csv(file_path)
+    print(f"✅ Read {len(df)} new claims")
+    return df
+
+def make_predictions(model, df):
+    print("Asking the magic box for predictions...")
+
+    predictions = model.predict(df)
+
+    df["prediction"] = predictions
+    print("✅ Predictions done!")
+    return df
 
 if __name__ == "__main__":
     print("Starting Prediction...")
