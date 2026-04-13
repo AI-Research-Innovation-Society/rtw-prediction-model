663513d3aff0deb0b5c940085b0015d35fedd48f
Ishan3131 <chetwanikrish@gmail.com>
2026-04-13 18:22:40 +0530

    feat: add baseline classification model

--- src/train.py
+++ src/train.py
@@ -1,1 +1,14 @@
 # TODO: see Issue #5
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+
+def train_model(X_train, y_train):
+    model = RandomForestClassifier(n_estimators=200, class_weight='balanced', random_state=42)
+    model.fit(X_train, y_train)
+    return model
+
+def save_model(model, path):
+    joblib.dump(model, path)
+
+def load_model(path):
+    return joblib.load(path)
